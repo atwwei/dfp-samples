8ab992d4fa2528b625eaa52f4f0872ef33d3df0b
王卫 <atwwei@gmail.com>
2023-01-19 16:31:19 +0800

    Fix compatibility issues

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
+import { NgModuleRef } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+declare var window: Record<string, any>;
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((ref: NgModuleRef<AppModule>) => {
+    // Ensure Angular destroys itself on hot reloads.
+    if (window['ngRef']) {
+      window['ngRef'].destroy();
+    }
+    window['ngRef'] = ref;
+  })
+  .catch((err) => console.error(err));
